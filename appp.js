8c73557da8dda7e72e24c47b97fb927182c145c3
devCoderian <devCoderian@gmail.com>
2021-09-05 23:53:44 +0900

    travelpush

--- appp.js
+++ appp.js
@@ -1,3 +1,7 @@
+// const convert = require('xml-js');
+import  { convert}  from'xml-js';
+import  { request } from 'request';
+// import  * from 'xml-js';
 // const serviceKey = "v0Xi5lEsrLZd5JFZMKiz77Ps1LszbA9CsAOfAXjQTTOBDJhwKoyLwB5mbO0RcRv0gAIh2RpJYkNaelVmlE5zYg%3D%3D";
 // const url ="http://api.visitkorea.or.kr/openapi/service/rest/KorService/searchKeyword?serviceKey="
 //     +serviceKey+"&MobileApp=AppTest&MobileOS=ETC&numOfRows=9999&listYN=Y&arrange=A&contentTypeId=12";
@@ -18,36 +22,29 @@
 //   }
 //   };
 
-  const serviceKey = "v0Xi5lEsrLZd5JFZMKiz77Ps1LszbA9CsAOfAXjQTTOBDJhwKoyLwB5mbO0RcRv0gAIh2RpJYkNaelVmlE5zYg%3D%3D";
-  const url ="https://dapi.kakao.com/v3/search/book?target=title"
-  const api = async (keyword = "") => {
-  
-    try {
-      console.log(`${url}&keyword=${keyword}`);
-      const response = await fetch(`${url}&query=${keyword}`,{
-        headers: {"Authorization: KakaoAK {REST_API_KEY}"}});
-      // const responseBody = await response.text();
-      const responseBody = await response.text();
-      const result = convert.xml2json(responseBody, {compact: true, spaces: 4});
-      // var result1 = convert.xml2json(responseBody, {compact: true, spaces: 4});
-      console.log(result)
-  
-      return responseBody;
-    } catch (error) {
-    // window.alert("API 서버에 문제가 발생했습니다.");
+
+
+
+const SERVICE_KEY = "v0Xi5lEsrLZd5JFZMKiz77Ps1LszbA9CsAOfAXjQTTOBDJhwKoyLwB5mbO0RcRv0gAIh2RpJYkNaelVmlE5zYg%3D%3D";
+const requestUrl ="http://api.visitkorea.or.kr/openapi/service/rest/KorService/searchKeyword?serviceKey="
+    +SERVICE_KEY+"&MobileApp=AppTest&MobileOS=ETC&numOfRows=9999&listYN=Y&arrange=A&contentTypeId=12";
+
+request.get(requestUrl, (err,res,body) =>{
+    if(err){
+        console.log(`err => ${err}`)
     }
-    };
-  
-    fetch("http://10.58.5.227:8000/account/sign-up",{
-      method : "POST",
-         body : JSON.stringify({
-               user_account : this.state.id,
-               password : this.state.password
-            })
-    })
-  export {
-    api,
-  };
+    else {
+        if(res.statusCode == 200){
+            var result = body
+            console.log(`body data => ${result}`)
+            var xmlToJson = convert.xml2json(result, {compact: true, spaces: 4});
+            console.log(`xml to json => ${xmlToJson}`)
+
+        }
+
+    }
+})
+
 
 
 
